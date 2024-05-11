2871f83efd4a4a4773ae614754b0891d1c8058a3
ZefanyaManurunggg <olanrimi@gmail.com>
2024-05-11 20:07:05 +0700

    asesmen2

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Asesmen2"
+include(":app")
+ 
